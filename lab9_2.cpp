6f773d5f0bbf1a5695ce7b4bc6ca25c846e10503
unknown <sunantha1881@gmail.com>
2020-01-28 17:22:05 +0700

    817

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,23 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+    ifstream cheerbook;
+    cheerbook.open("cheerbook.txt");
+    ofstream dest("D:\\261102\\lab9-2562-2-aewsunantha\\cheerbook_copy.txt");
+    string text;
+    dest<<"-------------------- SOTUS ---------------------"<<"\n";
+    while (getline(cheerbook,text)){
+        dest<<text<<"\n";
+       
+    }
+   dest<<"-------------------- SOTUS ---------------------";
+   dest.close();
+   return 0;
+}
+    
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,35 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cmath>
+using namespace std;
+int main(){
+    int count=0;
+    float sum=0;
+    float sum2=0;
+    float Mean;
+    string text;
+    ifstream score("score.txt");
+    while(getline(score,text)){
+
+    
+    cout<<text<<"\n";
+    sum += atof(text.c_str());
+    sum2 += pow(atof(text.c_str()),2);
+    count++;
+    }
+    
+
+
+
+Mean=sum/count;
+cout<<"Number of data = "<< count <<"\n";
+cout<<"Meam = "<< Mean<<'\n';
+cout<<"Standard deviation = "<< sqrt((sum2/count)-pow(Mean,2))<<'\n';
+score.close();
+return 0;
+}
